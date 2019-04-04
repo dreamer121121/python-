4830585fb743b28b9b9a4507df0992bae13ad1f2
dreamer <2761564455@qq.con>
2019-04-04 11:18:39 +0800

    角点检测

--- 图像导数.py
+++ 图像导数.py
@@ -3,8 +3,7 @@ from numpy import *
 from scipy.ndimage import filters
 from pylab import *
 
-im = array(Image.open("learn.jpg").convert("L"))
-
+im = array(Image.open("empire.jpg").convert("L"))
 #sobel导数滤波器
 # imx = zeros(im.shape)
 # filters.sobel(im,1,imx)
@@ -14,7 +13,19 @@ im = array(Image.open("learn.jpg").convert("L"))
 #
 # magnitude = sqrt(imx**2+imy**2)
 #
-# imshow(Image.fromarray(imx).convert("L"))
+# imshow(Image.fromarray(magnitude).convert("L"))
 # show()
 
 #高斯导数滤波器（注意与高斯平滑滤波器相比较）
+
+sigma = 2
+imx = zeros(im.shape)
+imx = filters.gaussian_filter(im,(sigma,sigma),(0,1),output=imx)
+
+imy = zeros(im.shape)
+imy = filters.gaussian_filter(im,(sigma,sigma),(1,0),output=imy)
+
+magnitude = sqrt(imx**2+imy**2)
+imshow(Image.fromarray(uint8(magnitude)))
+show()
+
