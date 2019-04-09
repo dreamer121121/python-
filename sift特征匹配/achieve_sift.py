532778efcc0d35000264ac67ce1c0210fe3642c4
Tao <=>
2019-04-09 18:42:18 +0800

    完成sift样例

---
+++ sift特征匹配/achieve_sift.py
@@ -0,0 +1,15 @@
+import sift
+from PIL import Image
+from pylab import *
+
+imname =  'empire.jpg'
+im1 = array(Image.open(imname).convert('L'))
+sift.process_image(imname,'empire.sift')
+l1,d1 = sift.read_features_from_file('empire.sift')
+
+print("--l1--",l1)
+
+figure()
+gray()
+sift.plot_features(im1,l1,circle=True)
+show()
