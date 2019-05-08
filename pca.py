e74e31784368c8997de73c7c1c3353e1ea494fe2
dreamer <2761564455@qq.con>
2019-05-08 22:55:52 +0800

    给pca添加注解

--- pca.py
+++ pca.py
@@ -10,25 +10,34 @@ def pca(X):
     
     # get dimensions
     num_data,dim = X.shape
-    
+
+    print("---X---",X.shape)
     # center data
     mean_X = X.mean(axis=0)
     X = X - mean_X
     
-    if dim>num_data:
+    # if dim>num_data:
         # PCA - compact trick used
-        M = dot(X,X.T) # covariance matrix
-        e,EV = linalg.eigh(M) # eigenvalues and eigenvectors
-        tmp = dot(X.T,EV).T # this is the compact trick
-        V = tmp[::-1] # reverse since last eigenvectors are the ones we want
-        S = sqrt(e)[::-1] # reverse since eigenvalues are in increasing order
-        for i in range(V.shape[1]):
-            V[:,i] /= S
-    else:
-        # PCA - SVD used
-        U,S,V = linalg.svd(X)
-        V = V[:num_data] # only makes sense to return the first num_data
-    
+    M = dot(X,X.T) # covariance matrix
+    print("---协方差矩阵---",M.shape)
+    e,EV = linalg.eigh(M) # eigenvalues and eigenvectors
+    print("---e---",e.shape)
+    print("--EV--",EV.shape)
+    tmp = dot(X.T,EV).T # this is the compact trick
+    print("--tmp--",tmp.shape)
+    V = tmp[::-1] # reverse since last eigenvectors are the ones we want
+    S = sqrt(e)[::-1] # reverse since eigenvalues are in increasing order
+    for i in range(V.shape[1]):
+        V[:,i] /= S
+    # else:
+    #     print("---SVD---")
+    #     # PCA - SVD used
+    #     U,S,V = linalg.svd(X) #进行SVD分解
+    #     print("---S.shape--",S.shape)
+    #     print("---V.shape--",V.shape)
+    #     print("---U.shape--",U.shape)
+    #     V = V[:num_data] # only makes sense to return the first num_data
+    #
     # return the projection matrix, the variance and the mean
     return V,S,mean_X
 
@@ -56,7 +65,7 @@ from pylab import *
 import os
 
 def read_file():
-    path = r'C:\Users\Tao xia\Desktop\python-计算机视觉\data\fontimages\a_thumbs'
+    path = r'C:\Users\dreamer\Desktop\reposite\python计算机视觉\data\fontimages\a_thumbs'
     ims = [os.path.join(path,f) for f in os.listdir(path) if f.endswith(".jpg")]
     return ims
 
