9af425a3fe69d68ced0bfc5fe7e7cff6ad9eec9b
dreamer <2761564455@qq.con>
2019-05-29 23:02:13 +0800

    添加构建VOC的关键注释

--- cbir/cbir.py
+++ cbir/cbir.py
@@ -25,6 +25,7 @@ voc.train(featlist,1000,10)
 
 #保存词汇
 with open('vocabulary.pkl','wb') as f:
-    pickle.dump(voc,f)
+    pickle.dump(voc,f) #注意此处的voc是一个对象，我们暂时将其以字符串形式保存在文件中
+#以后每当我们需要使用voc的属性时，我们将其从文件中读出来从新实例化变可使用其属性和方法。
 
 print('vocabulary is :',voc.name,voc.nbr_words)
--- cbir/vocabulary.py
+++ cbir/vocabulary.py
@@ -26,35 +26,36 @@ class Vocabulary(object):
         for i in arange(1,nbr_images):
             print("--i--",i)
             descr.append(sift.read_features_from_file(featurefiles[i])[1])
-            descriptors = vstack((descriptors,descr[i]))
+            descriptors = vstack((descriptors,descr[i])) #按列方向将矩阵进行拼接
 
         # k-means: last number determines number of runs
-        self.voc,distortion = kmeans(descriptors[::subsampling,:],k,1)
-        self.nbr_words = self.voc.shape[0]
-        print("--voc.shpe--",self.nbr_words)
+        self.voc,distortion = kmeans(descriptors[::subsampling,:],k,10)
+        self.nbr_words = self.voc.shape[0] #voc.shape= (1000,128)
+        #self.voc就是一个单词表长度为1000，每一个单词就是一个128维的向量
 
-        # # go through all training images and project on vocabulary
-        # imwords = zeros((nbr_images,self.nbr_words))
-        # for i in range( nbr_images ):
-        #     imwords[i] = self.project(descr[i])
-        #
-        # nbr_occurences = sum( (imwords > 0)*1 ,axis=0)
-        #
-        # self.idf = log( (1.0*nbr_images) / (1.0*nbr_occurences+1) )
-        # self.trainingdata = featurefiles
+        # go through all training images and project on vocabulary
+        imwords = zeros((nbr_images,self.nbr_words)) #imwords是（图片数量X单词表长度）
+        for i in range( nbr_images ):
+            "一张一张图片进行投影"
+            imwords[i] = self.project(descr[i])
+
+        nbr_occurences = sum( (imwords > 0)*1 ,axis=0)
+
+        self.idf = log( (1.0*nbr_images) / (1.0*nbr_occurences+1) ) #计算idf
+        self.trainingdata = featurefiles
 
     def project(self,descriptors):
         """ Project descriptors on the vocabulary
             to create a histogram of words. """
-        
+
         # histogram of image words 
         imhist = zeros((self.nbr_words))
-        words,distance = vq(descriptors,self.voc)
+        words,distance = vq(descriptors,self.voc) #注意words是index
         for w in words:
             imhist[w] += 1
-        
+
         return imhist
-    
+
     def get_words(self,descriptors):
         """ Convert descriptors to words. """
         return vq(descriptors,self.voc)[0]
